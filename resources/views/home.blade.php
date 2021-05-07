c6d5a17426410031517d1cff7b856346ac06e6e1
Filippo Garota <filogarota101@gmail.com>
2021-05-07 17:00:28 +0200

    Stampo le paste per ogni categoria

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,39 @@
     </div>
   </header>
   <main>
-    
+    <div class="container">
+    {{-- sezione pasta lunga --}}
+    <section>
+      <h2>Le lunghe</h2>
+      <div class="cnt__cards">
+        @foreach ($lunga as $pasta)
+          <div class="card">
+            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+          </div>  
+        @endforeach
+      </div>
+    </section>
+    {{-- sezione per la pasta corta --}}
+    <section>
+      <section>
+      <h2>Le corte</h2>
+      <div class="cnt__cards">
+       @foreach ($corte as $pasta)
+           <div class="card">
+            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+          </div>  
+       @endforeach
+    </section>
+    <section>
+       <h2>Le cortissime</h2>
+      <div class="cnt__cards">
+       @foreach ($cortissima as $pasta)
+           <div class="card">
+            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+          </div>  
+       @endforeach
+    </section>
+    </div>
   </main>
   <footer></footer>
 </body>
