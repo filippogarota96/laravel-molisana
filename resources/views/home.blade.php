81e815124d1c7d3e1255cc1e4c2738f941b28213
Filippo Garota <filogarota101@gmail.com>
2021-05-07 16:20:03 +0200

    Stile dell'header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,18 @@
 </head>
 <body>
   <header>
-    <div class="logo">
-      <img src="{{asset('images/marchio-sito-test.png')}}" alt="Logo La Molsana">
+    <div class="container">
+      <div class="logo">
+        <img src="{{asset('images/marchio-sito-test.png')}}" alt="Logo La Molsana">
+      </div>
+      <nav class="main-nav">
+        <ul>
+          <li><a href="#" class="active">Home</a></li>
+          <li><a href="#">Prodotti</a></li>
+          <li><a href="#">News</a></li>
+
+        </ul>
+      </nav>
     </div>
   </header>
   <main></main>
